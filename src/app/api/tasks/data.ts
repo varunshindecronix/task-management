957c7547132acd6fdd6e45879b93542c36aa3e4c
varunshindecronix <varun.shinde@cronixweb.com>
2025-07-17 23:39:02 +0530

    build success

--- src/app/api/tasks/data.ts
+++ src/app/api/tasks/data.ts
@@ -1,5 +1,5 @@
 // app/api/tasks/data.ts
-export let tasks = [
+export const tasks = [
   { id: 1, title: "Buy groceries", status: "pending" },
   { id: 2, title: "Write code", status: "done" },
 ];
